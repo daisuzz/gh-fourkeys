ccf0f38d83ad28f599bd798b0b494f4e17f77e4f
daisuzz <daisaku.rugby@gmail.com>
2023-04-16 11:48:38 +0900

    refactoring

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// 変更頻度が多いファイルを10個出力
 type fileCount struct {
 	filename string
 	count    int
@@ -40,6 +39,26 @@ func main() {
 	oAuthClient := oauth2.NewClient(ctx, tokenSource)
 	ghClient := github.NewClient(oAuthClient)
 
+	// PRのメトリクスを取得
+	mergedPRCount, timeToMerge, err := getPRMetrics(ghClient, ctx, owner, repo, base)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Merged PR count within 6 months: %d\n\n", mergedPRCount)
+	fmt.Printf("Average time to merge: %v\n\n", timeToMerge/time.Duration(mergedPRCount))
+
+	// リポジトリのコミット履歴を取得
+	fileList, err := getSortedFileList(ghClient, ctx, owner, repo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Top 10 changed files within 6 months:\n")
+	for i := 0; i < 10 && i < len(fileList); i++ {
+		fmt.Printf("%s: %d\n", fileList[i].filename, fileList[i].count)
+	}
+}
+
+func getPRMetrics(client *github.Client, ctx context.Context, owner, repo, base string) (int, time.Duration, error) {
 	options := &github.PullRequestListOptions{
 		State:     "closed",
 		Base:      base,
@@ -54,9 +73,9 @@ func main() {
 	var timeToMerge time.Duration
 
 	for {
-		prs, res, err := ghClient.PullRequests.List(ctx, owner, repo, options)
+		prs, res, err := client.PullRequests.List(ctx, owner, repo, options)
 		if err != nil {
-			log.Fatal(err)
+			return 0, 0, err
 		}
 
 		for _, pr := range prs {
@@ -76,18 +95,7 @@ func main() {
 		options.Page = res.NextPage
 	}
 
-	fmt.Printf("Merged PR count within 6 months: %d\n\n", mergedPRCount)
-	fmt.Printf("Average time to merge: %v\n\n", timeToMerge/time.Duration(mergedPRCount))
-
-	// リポジトリのコミット履歴を取得
-	fileList, err := getSortedFileList(ghClient, ctx, owner, repo)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Top 10 changed files within 6 months:\n")
-	for i := 0; i < 10 && i < len(fileList); i++ {
-		fmt.Printf("%s: %d\n", fileList[i].filename, fileList[i].count)
-	}
+	return mergedPRCount, timeToMerge, nil
 }
 
 func getSortedFileList(client *github.Client, ctx context.Context, owner, repo string) ([]fileCount, error) {
